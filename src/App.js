4f79ebf4c2e117ab266d09802126687d2bb5df58
Tom Lou <louyu27@cs.washington.edu>
2019-12-20 15:51:59 -0800

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   render = () => {
     return (
-      <div>
+      <div className="container">
         <Route exact path="/settings">
           <Settings
             onListChange={this.onListChange}
