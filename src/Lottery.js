867992eabf423c2ba8516ea4cfd7566074a1899c
Tom Lou <louyu27@cs.washington.edu>
2019-12-19 22:26:40 -0800

    Finised start and stop

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-class Lottery extends React.Component {
-    state = {running: false,
-        currentWinner: "ab"};
-    
-    
+let updateInterval = 50;
 
-    render = () => {
-        return (
-        <div>
-            <p>
-                {this.state.currentWinner?
-                    this.state.currentWinner:
-                    "Please Press Start"}
-            </p>
-            <button>Start</button>
-            <button>Save</button>
-            <Link to="/settings">
-                <button>Settings</button>
-            </Link>
-        </div>)
+let randrange = (n) => {
+  return Math.floor(Math.random()*n);
+}
+
+class Lottery extends React.Component {
+	state = {running: false,
+    currentWinner: "ab"};
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.running && !prevState.running) {
+      this.onWinnerUpdate();
     }
+  }
+    
+	onWinnerUpdate = () => {
+    let winner;
+    let participants = this.props.participants;
+    let winners = this.props.winners;
+    if (winners.length >= participants.length) {
+      return;
+    }
+
+		do {
+			winner = participants[randrange(participants.length)];
+		} while (winners.includes(winner));
+		this.setState({currentWinner: winner});
+		if (this.state.running) {
+      setTimeout(this.onWinnerUpdate, updateInterval);
+		}
+  }
+
+	render = () => {
+		return (
+		<div>
+			<p>
+				{this.state.currentWinner?
+					this.state.currentWinner:
+					"Please Press Start"}
+			</p>
+			<button onClick={() => this.setState({running: true})}>
+        Start
+      </button>
+			<button onClick={() => this.setState({running: false})}>
+        Stop
+      </button>
+			<button>Save</button>
+			<Link to="/settings">
+				<button>Settings</button>
+			</Link>
+		</div>)
+	}
 }
 
 export default Lottery;
