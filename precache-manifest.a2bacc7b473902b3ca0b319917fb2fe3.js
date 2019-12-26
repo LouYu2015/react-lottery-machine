a72f55e8c915458e337bb28b75c8ca56a30981de
Tom Lou <louyu27@cs.washington.edu>
2019-12-25 22:07:40 -0800

    Updates

--- precache-manifest.a2bacc7b473902b3ca0b319917fb2fe3.js
+++ precache-manifest.a2bacc7b473902b3ca0b319917fb2fe3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "31e29bff08928f244ed210cf5bc9723a",
+    "revision": "1ccdb5cf7e898ddda249638b4f133fbf",
     "url": "/react-lottery-machine/index.html"
   },
   {
-    "revision": "62e9374eedc1f41c33ae",
+    "revision": "92195f474c7761787d67",
     "url": "/react-lottery-machine/static/css/main.397e25c3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-lottery-machine/static/js/2.c1da45e4.chunk.js.LICENSE"
   },
   {
-    "revision": "62e9374eedc1f41c33ae",
-    "url": "/react-lottery-machine/static/js/main.6a485477.chunk.js"
+    "revision": "92195f474c7761787d67",
+    "url": "/react-lottery-machine/static/js/main.83ed67e9.chunk.js"
   },
   {
     "revision": "e785149c392f75b2c034",
